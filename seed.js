1b8a905d3782f5825d7897d73566812d4c6aace5
byronholmes2022 <byronholmes2022@gmail.com>
2025-02-17 19:05:58 -0600

    first commit

---
+++ seed.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const { client, createTables, createWidget, fetchWidgets } = require("./db.js");
+
+const seedDb = async () => {
+  try {
+    await client.connect();
+    await createTables();
+    console.log("Creating widgets...");
+    const [widget1, widget2, widget3] = await Promise.all([
+      createWidget("machinator"),
+      createWidget("code combobulator"),
+      createWidget("carpet vencillator"),
+    ]);
+    console.log("Widgets created!");
+    console.log("Here are all the widgets...");
+    console.log(await fetchWidgets());
+  } catch (err) {
+    console.error(err);
+  } finally {
+    client.end();
+  }
+};
+
+seedDb();
